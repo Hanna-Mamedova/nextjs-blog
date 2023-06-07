87353f38a7b38af7d30c5ed0613d85d939a35428
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-06-07 22:31:58 +0200

    feat: add custom theme and override tw colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,13 +32,13 @@ export default function Home({ allPostsData }: HomeProps) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className='font-condensed'>[Your Self Introduction]</p>
+        <p className='font-condensed p-3.5'>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you will be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px} bg-light-blue`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,36 @@ module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false,
     theme: {
+        colors: {
+            'blue': '#1DA1F2',
+            'dark-blue': '#2795D9',
+            'light-blue': '#EFF9FF',
+            'red': '#E0245E',
+            'dark-red': '#B21E53',
+            'light-red': '#FEDDE2',
+        },
         extend: {
             fontFamily: {
                 "slab": ['RobotoSlab', 'ui-sans-serif', 'system-ui'],
                 "condensed": ['RobotoCondensed', 'ui-serif', 'Georgia'],
+            },
+            colors: {
+                'custom-blue': '#247BA0',
+                'custom-green': '#70C1B3',
+                'custom-yellow': '#B2DBBF',
+                'custom-red': '#F25F5C'
+            },
+            spacing: {
+                '1/2': '50%',
+                '1/3': '33.333333%',
+                '2/3': '66.666667%',
+                '1/4': '25%',
+                '2/4': '50%',
+                '3/4': '75%',
+                '3.5': '0.875rem',
+                '4.5': '1.125rem',
+                '5.5': '1.375rem',
+                '9.5': '2.375rem',
             }
         },
     },
